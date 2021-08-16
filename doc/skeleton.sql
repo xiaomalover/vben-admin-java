061ef3d8dc269a4b535a1d7e1a9c11d804cbcc3a
xm <xm@lfcs.com>
2021-08-16 12:46:13 +0800

    update

--- doc/skeleton.sql
+++ doc/skeleton.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v12.5.0 (64 bit)
-MySQL - 8.0.20 : Database - skeleton
+MySQL - 5.6.50-log : Database - skeleton
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 8.0.20 : Database - skeleton
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`skeleton` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`skeleton` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `skeleton`;
 
@@ -21,22 +21,22 @@ USE `skeleton`;
 DROP TABLE IF EXISTS `sys_admin`;
 
 CREATE TABLE `sys_admin` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '密码',
-  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '头像',
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '描述',
-  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
-  `mobile` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
-  `nickname` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '昵称',
-  `sex` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '性别：0男;1女',
-  `status` tinyint unsigned NOT NULL DEFAULT '1' COMMENT '状态(0无效;1有效)',
-  `role_id` int unsigned NOT NULL DEFAULT '0' COMMENT '角色ID',
-  `department_id` int unsigned NOT NULL DEFAULT '0' COMMENT '部门ID',
-  `created_at` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `created_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '创建人',
-  `updated_at` int unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
-  `updated_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `username` varchar(16) NOT NULL COMMENT '用户名',
+  `password` varchar(64) NOT NULL DEFAULT '' COMMENT '密码',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '描述',
+  `email` varchar(64) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `mobile` varchar(32) NOT NULL DEFAULT '' COMMENT '手机号',
+  `nickname` varchar(64) NOT NULL DEFAULT '' COMMENT '昵称',
+  `sex` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '性别：0男;1女',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态(0无效;1有效)',
+  `role_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '角色ID',
+  `department_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '部门ID',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `updated_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(32) NOT NULL DEFAULT '' COMMENT '修改人',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='管理员表';
@@ -53,18 +53,18 @@ insert  into `sys_admin`(`id`,`username`,`password`,`avatar`,`description`,`emai
 DROP TABLE IF EXISTS `sys_admin_log`;
 
 CREATE TABLE `sys_admin_log` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `cost_time` int unsigned NOT NULL DEFAULT '0' COMMENT '请求处理时长',
-  `ip` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '请求ip地址',
-  `ip_info` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '请求ip信息',
-  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '日志名称',
-  `request_param` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '请求参数',
-  `request_type` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '请求类型',
-  `request_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '请求地址',
-  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '操作人',
-  `created_at` int unsigned NOT NULL DEFAULT '0' COMMENT '操作时间',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `cost_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '请求处理时长',
+  `ip` varchar(64) NOT NULL DEFAULT '' COMMENT '请求ip地址',
+  `ip_info` varchar(255) NOT NULL DEFAULT '' COMMENT '请求ip信息',
+  `name` varchar(64) NOT NULL DEFAULT '' COMMENT '日志名称',
+  `request_param` varchar(512) NOT NULL DEFAULT '' COMMENT '请求参数',
+  `request_type` varchar(16) NOT NULL DEFAULT '' COMMENT '请求类型',
+  `request_url` varchar(255) NOT NULL DEFAULT '' COMMENT '请求地址',
+  `username` varchar(32) NOT NULL DEFAULT '' COMMENT '操作人',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '操作时间',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='请求日志';
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='请求日志';
 
 /*Data for the table `sys_admin_log` */
 
@@ -79,23 +79,24 @@ insert  into `sys_admin_log`(`id`,`cost_time`,`ip`,`ip_info`,`name`,`request_par
 (8,37,'192.168.56.1','','登录系统','{\"password\":\"******\",\"captcha\":\"uqjq\",\"captchaId\":\"34fe7b40d603483ca81c6ca1a8bb9cc7\",\"username\":\"admin\"}','POST','/skeleton/login','admin',1628836529),
 (9,44,'192.168.56.1','','登录系统','{\"password\":\"******\",\"captcha\":\"cjkz\",\"captchaId\":\"7f31f064e79b40fe9564ff4132c87f91\",\"username\":\"admin\"}','POST','/skeleton/login','admin',1628856515),
 (10,3,'192.168.56.1','','登录系统','{\"password\":\"******\",\"captcha\":\"kilt\",\"captchaId\":\"3b9cdf7ce6f04dccbfbf92f1906f528e\",\"username\":\"admin\"}','POST','/skeleton/login','admin',1628860258),
-(11,39,'192.168.56.1','','登录系统','{\"password\":\"******\",\"captcha\":\"fi6y\",\"captchaId\":\"2db8e4b1500640e5984045a3daaca240\",\"username\":\"admin\"}','POST','/skeleton/login','admin',1628996765);
+(11,39,'192.168.56.1','','登录系统','{\"password\":\"******\",\"captcha\":\"fi6y\",\"captchaId\":\"2db8e4b1500640e5984045a3daaca240\",\"username\":\"admin\"}','POST','/skeleton/login','admin',1628996765),
+(12,19,'192.168.56.1','','登录系统','{\"password\":\"******\",\"captcha\":\"9dgy\",\"captchaId\":\"f6a6f297a21847d5be8b741dd06de69f\",\"username\":\"admin\"}','POST','/skeleton/login','admin',1629087977);
 
 /*Table structure for table `sys_department` */
 
 DROP TABLE IF EXISTS `sys_department`;
 
 CREATE TABLE `sys_department` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `parent_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '上级部门',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `parent_id` varchar(32) NOT NULL DEFAULT '' COMMENT '上级部门',
   `name` varchar(32) NOT NULL DEFAULT '' COMMENT '部门名称',
-  `description` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '描述',
+  `description` varchar(64) NOT NULL DEFAULT '' COMMENT '描述',
   `sort_order` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '排序',
-  `status` tinyint unsigned NOT NULL DEFAULT '1' COMMENT '状态(0无效;1有效)',
-  `created_at` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `created_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '创建人',
-  `updated_at` int unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
-  `updated_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态(0无效;1有效)',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `updated_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(32) NOT NULL DEFAULT '' COMMENT '修改人',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
@@ -119,70 +120,81 @@ insert  into `sys_department`(`id`,`parent_id`,`name`,`description`,`sort_order`
 DROP TABLE IF EXISTS `sys_permission`;
 
 CREATE TABLE `sys_permission` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `type` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '类型：0目录;1菜单;按钮',
-  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '名称',
-  `parent_id` int unsigned NOT NULL DEFAULT '0' COMMENT '父类ID',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '类型：0目录;1菜单;按钮',
+  `name` varchar(64) NOT NULL DEFAULT '' COMMENT '名称',
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父类ID',
   `sort_order` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '排序',
-  `icon` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '图标',
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '路由地址',
-  `component` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '组件路径',
-  `permision_code` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '权限标识',
-  `status` tinyint unsigned NOT NULL DEFAULT '1' COMMENT '状态(0无效;1有效)',
-  `is_display` tinyint unsigned NOT NULL DEFAULT '1' COMMENT '是否显示',
-  `is_external_link` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '是否外链',
-  `is_cache` tinyint unsigned DEFAULT '0' COMMENT '是否缓存',
-  `created_at` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `created_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '创建人',
-  `updated_at` int unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
-  `updated_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `icon` varchar(255) NOT NULL DEFAULT '' COMMENT '图标',
+  `url` varchar(255) NOT NULL DEFAULT '' COMMENT '路由地址',
+  `component` varchar(255) NOT NULL DEFAULT '' COMMENT '组件路径',
+  `permision_code` varchar(128) NOT NULL DEFAULT '' COMMENT '权限标识',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态(0无效;1有效)',
+  `is_display` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否显示',
+  `is_external_link` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否外链',
+  `is_cache` tinyint(3) unsigned DEFAULT '0' COMMENT '是否缓存',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `updated_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(32) NOT NULL DEFAULT '' COMMENT '修改人',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='菜单权限表';
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='菜单权限表';
 
 /*Data for the table `sys_permission` */
 
 insert  into `sys_permission`(`id`,`type`,`name`,`parent_id`,`sort_order`,`icon`,`url`,`component`,`permision_code`,`status`,`is_display`,`is_external_link`,`is_cache`,`created_at`,`created_by`,`updated_at`,`updated_by`) values 
-(1,0,'耍花枪',0,1.00,'ant-design:account-book-twotone','sda as as','','',0,0,0,0,1628997675,'admin',1628997675,'admin');
+(1,0,'系统管理',0,1.00,'ant-design:setting-outlined','/system','','',0,0,0,0,1629079245,'admin',1629079245,'admin'),
+(2,1,'账号管理',1,1.00,'ant-design:usergroup-add-outlined','/system/account','','system:account',0,0,0,1,1629088689,'admin',1629088689,'admin'),
+(3,1,'角色管理',1,2.00,'ant-design:experiment-filled','system/role','','system:role',0,0,0,1,1629088812,'admin',1629088812,'admin'),
+(4,1,'菜单管理',1,3.00,'ant-design:unordered-list-outlined','system/menu','','system:menu',0,0,0,1,1629088897,'admin',1629088897,'admin'),
+(5,1,'部门管理',1,3.00,'ant-design:appstore-filled','system/dept','','system:dept',0,0,0,1,1629088944,'admin',1629088944,'admin');
 
 /*Table structure for table `sys_role` */
 
 DROP TABLE IF EXISTS `sys_role`;
 
 CREATE TABLE `sys_role` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '角色名称',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `name` varchar(32) NOT NULL DEFAULT '' COMMENT '角色名称',
   `code` varchar(32) NOT NULL DEFAULT '' COMMENT '角色值',
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '角色描述',
-  `status` tinyint unsigned NOT NULL DEFAULT '1' COMMENT '状态：0停用;1启用',
-  `created_at` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `created_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '创建人',
-  `updated_at` int unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
-  `updated_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '角色描述',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态：0停用;1启用',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `updated_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(32) NOT NULL DEFAULT '' COMMENT '修改人',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='角色表';
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 /*Data for the table `sys_role` */
 
 insert  into `sys_role`(`id`,`name`,`code`,`description`,`status`,`created_at`,`created_by`,`updated_at`,`updated_by`) values 
 (1,'测试角色','ROLE_TEST','测试角色',1,1628833064,'admin',1628835553,'admin'),
-(2,'超级管理员','ROLE_ADMIN','超级管理员',1,1628833105,'admin',1628835546,'admin');
+(9,'超级管理员','ROLE_ADMIN','超级管理员角色',1,1629089045,'admin',1629089055,'admin');
 
 /*Table structure for table `sys_role_permission` */
 
 DROP TABLE IF EXISTS `sys_role_permission`;
 
 CREATE TABLE `sys_role_permission` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `permission_id` int unsigned NOT NULL DEFAULT '0' COMMENT '权限ID',
-  `role_id` int unsigned NOT NULL DEFAULT '0' COMMENT '角色ID',
-  `created_at` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `created_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '创建人',
-  `updated_at` int unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
-  `updated_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `permission_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '权限ID',
+  `role_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '角色ID',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(32) NOT NULL DEFAULT '' COMMENT '创建人',
+  `updated_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(32) NOT NULL DEFAULT '' COMMENT '修改人',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='角色权限关联表';
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='角色权限关联表';
 
 /*Data for the table `sys_role_permission` */
+
+insert  into `sys_role_permission`(`id`,`permission_id`,`role_id`,`created_at`,`created_by`,`updated_at`,`updated_by`) values 
+(10,5,9,1629089055,'admin',1629089055,'admin'),
+(9,4,9,1629089055,'admin',1629089055,'admin'),
+(8,3,9,1629089055,'admin',1629089055,'admin'),
+(7,2,9,1629089055,'admin',1629089055,'admin'),
+(6,1,9,1629089055,'admin',1629089055,'admin');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
