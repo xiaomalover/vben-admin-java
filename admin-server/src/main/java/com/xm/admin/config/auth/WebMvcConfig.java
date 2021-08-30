936e97650d89b85e914dfb12d4290c31a784ab90
xm <xm@lfcs.com>
2021-08-30 16:17:43 +0800

    update

---
+++ admin-server/src/main/java/com/xm/admin/config/auth/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.xm.admin.config.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * <p>
+ * MVC配置
+ * </p>
+ *
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE").maxAge(MAX_AGE_SECS);
+    }
+}
